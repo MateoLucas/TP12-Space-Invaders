94e2584a8e00066daef420e43f91bc1ff4954fa6
Mdolhare <mdolhare@itba.edu.ar>
2021-02-19 14:40:53 -0300

    LOL

--- TP12_Space_Invaders/ship.h
+++ TP12_Space_Invaders/ship.h
@@ -19,6 +19,15 @@
 #define SHIP_MAX_X (BUFFER_W - SHIP_W)
 #define SHIP_MAX_Y (BUFFER_H - SHIP_H)
 
+typedef struct SHIP
+{
+    int x, y;
+    int shot_timer;
+    int lives;
+    int respawn_timer;
+    int invincible_timer;
+} SHIP;
+
 void ship_init();
 void ship_update();
 void ship_draw();
