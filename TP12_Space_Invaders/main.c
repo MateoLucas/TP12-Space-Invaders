4faf3e78fdf7e1b5cc4190efe10674f6679789da
MateoLucas <mlucas@itba.edu.ar>
2021-03-05 10:46:42 -0300

    05/03

--- TP12_Space_Invaders/main.c
+++ TP12_Space_Invaders/main.c
@@ -2,14 +2,7 @@
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <allegro5/allegro5.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
-#include <allegro5/allegro_image.h>
+#include "allegro.h"
 #include "aliens.h"
 #include "ship.h"
 #include "fx.h"
